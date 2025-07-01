9886633b07d4b62c998e5ed11e67b7e9d7f27c10
EZZAHIRTAHA <taha1.ezzahir@gmail.com>
2025-07-01 15:02:38 +0100

    cred

--- resources/views/contact_us.blade.php
+++ resources/views/contact_us.blade.php
@@ -17,7 +17,7 @@
                             Name</label>
                         <input type="email" id="email"
                             class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                            placeholder="Mohamed" required>
+                            placeholder="TAHA" required>
                     </div>
 
                     <div class="w-full ">
@@ -25,7 +25,7 @@
                             Name</label>
                         <input type="email" id="email"
                             class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                            placeholder="Galdi" required>
+                            placeholder="MOUAD" required>
                     </div>
                 </div>
                 <div class="flex justify-between">
@@ -34,7 +34,7 @@
                             class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email</label>
                         <input type="email" id="email"
                             class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                            placeholder="contact.galdi@gmail.com" required>
+                            placeholder="soulaimane@gmail.com" required>
                     </div>
 
                     <div class="w-full ">
